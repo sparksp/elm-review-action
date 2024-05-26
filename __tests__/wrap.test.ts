a14203ec98d99f80c4d64838f8376c0cc53ea2df
Phill Sparks <hello@phills.me.uk>
2024-05-26 09:54:37 +0100

    Upgrade eslint and prettier

--- __tests__/wrap.test.ts
+++ __tests__/wrap.test.ts
@@ -9,21 +9,25 @@ test('it does not wrap paragraphs shorter than the given length', () => {
 test('it wraps paragraphs longer than the given length', () => {
   const input = 'The quick brown fox jumps over the lazy dog.'
   const result = wrap(10, input)
-  expect(result).toEqual('The quick\n\
+  expect(result).toEqual(
+    'The quick\n\
 brown fox\n\
 jumps over\n\
 the lazy\n\
-dog.')
+dog.'
+  )
 })
 
 test('it keeps new lines in paragraphs longer than the given length', () => {
   const input = 'The quick brown\nfox jumps over the lazy dog.'
   const result = wrap(10, input)
-  expect(result).toEqual('The quick\n\
+  expect(result).toEqual(
+    'The quick\n\
 brown\n\
 fox jumps\n\
 over the\n\
-lazy dog.')
+lazy dog.'
+  )
 })
 
 test('it maintains paragraphs shorter than the given length', () => {
